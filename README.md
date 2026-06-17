65bac46970cb490107f9ca865a01e8f6a965220e
Tang Piseth <tangpiseth43@gmail.com>
2026-06-17 14:57:22 +0930

    Guessing game

--- README.md
+++ README.md
@@ -4,3 +4,21 @@
 
 1. [Rust Project](https://codecrafters.io/blog/rust-projects)
 2. [Bogdan's 2nd video (important ...)](https://www.youtube.com/watch?v=H0xBSbnQYds&list=PLai5B987bZ9CoVR-QEIN9foz4QCJ0H2Y8&index=3)
+3. [How NOT to learn Rust](https://www.youtube.com/watch?v=2uY5tpcs3Gs)
+----> Learn 20% that will give us 80%.
+Examples:
+    1. Backend engineer (building dist system): async rust, tokio, serialization, deserialization (no need to be an expert in borrow checker, FFI, allocator, type state patterns, allocators, no_std Rust)
+
+2. Being a productive procrastinator:
+- productive failures 
+- 
+
+
+
+3. Thinking you can vibe-code Rust.
+- unable to debug code
+- unable to adapt to changing requirements
+- stuck when something breaks. 
+
+
+Fundamentals: ownership model, borrow checker, trait and generics. 
---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use std::io;
+use rand::Rng;
+
+use std::cmp::Ordering;
+
+
+use colored::*;
+
+
+
+
+
+
+fn main() {
+    println!("Guess the number!");
+    println!("Please input your guess");
+
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is: {}", secret_number);
+
+    loop {
+        // in rust, variables are immutable by default.
+        let mut guess = String::new();
+        
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+      
+
+        println!("You guessed: {}", guess);
+
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("{}", "Too small!".red()),
+                Ordering::Greater => println!("{}", "Too big!".green()),
+                Ordering::Equal => { 
+                    println!("You win!");
+                    break;
+                }
+            }   
+    }
+}
